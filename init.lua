06cb3e7a5f7ebd4b7e36ad3e94143b5f7dc13b42
@slovacus <slovacus@gmail.com>
2019-12-20 12:44:27 -0500

    (#3) chore: implement core and modules

---
+++ init.lua
@@ -0,0 +1,15 @@
+hs.loadSpoon("SpoonInstall")
+spoon.SpoonInstall.repos.doiken = {
+   url = "https://github.com/doiken/Spoons",
+   desc = "doiken's spoon repository",
+}
+require('modules.default')
+require('modules.memory')
+require('modules.speed')
+require('modules.widget')
+require('modules.tools')
+require('modules.windows')
+require('modules.switcher')
+require('modules.notification_center')
+require('modules.wifi')
+require('modules.apps')
