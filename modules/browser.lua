ed1a77e80b0ff5be0471f3550438d518d4010f7d
Luis Mayta <luis@hadenlabs.com>
2019-12-28 04:00:01 -0500

    chore: implement module browser (#10)

---
+++ modules/browser.lua
@@ -0,0 +1,19 @@
+local hotkey = require("core.hotkey")
+local settings = require("core.config")
+
+hs.loadSpoon("SpoonInstall")
+Install=spoon.SpoonInstall
+
+Install:andUse("URLDispatcher",
+    {
+        config = {
+            url_patterns = {
+                { "https?://endustria.atlassian.net/", settings.DefaultBrowser },
+                { "https?://bitbucket.org/dashboard/overview",settings.DefaultBrowser },
+                { "https?://github.com", settings.DefaultBrowser },
+            },
+            default_handler = DefaultBrowser
+        },
+        start = true
+    }
+)
