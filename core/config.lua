a055609be0fc2551425abdd00f7d104d60610964
@slovacus <slovacus@gmail.com>
2019-12-28 04:13:39 -0500

    fix: return data for configuration (#10)

--- core/config.lua
+++ core/config.lua
@@ -1,7 +1,10 @@
-DefaultBrowser = "com.electron.brave"
-CollabApp = DefaultBrowser
-DnsEmpty= "networksetup -setdnsservers Wi-Fi empty"
-DnsCloudflare= "networksetup -setdnsservers Wi-Fi 8.8.8.8 8.8.4.4 1.1.1.1 1.0.0.1"
+config = {
+    DefaultBrowser = "com.electron.brave",
+    CollabApp = DefaultBrowser,
+    DnsEmpty= "networksetup -setdnsservers Wi-Fi empty",
+    DnsCloudflare= "networksetup -setdnsservers Wi-Fi 8.8.8.8 8.8.4.4 1.1.1.1 1.0.0.1",
+}
 -- default_handler = "com.google.Chrome"
 -- default_handler = "com.electron.brave"
 -- default_handler = "com.brave.Browser.dev"
+return config
