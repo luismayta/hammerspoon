f4997561e8a1a8ffac66fd7d36924e416db8dd80
@slovacus <slovacus@gmail.com>
2019-12-28 03:59:25 -0500

    (#10) feat: implement config vars

---
+++ core/config.lua
@@ -0,0 +1,5 @@
+DefaultBrowser = "com.electron.brave"
+CollabApp = DefaultBrowser
+-- default_handler = "com.google.Chrome"
+-- default_handler = "com.electron.brave"
+-- default_handler = "com.brave.Browser.dev"
