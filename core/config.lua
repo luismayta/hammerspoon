6867a1abe943e70766b924151b3bc64d7d8a0e38
@slovacus <slovacus@gmail.com>
2019-12-28 04:06:20 -0500

    (#10) feat: implement settings for in core

--- core/config.lua
+++ core/config.lua
@@ -1,5 +1,7 @@
 DefaultBrowser = "com.electron.brave"
 CollabApp = DefaultBrowser
+DnsEmpty= "networksetup -setdnsservers Wi-Fi empty"
+DnsCloudflare= "networksetup -setdnsservers Wi-Fi 8.8.8.8 8.8.4.4 1.1.1.1 1.0.0.1"
 -- default_handler = "com.google.Chrome"
 -- default_handler = "com.electron.brave"
 -- default_handler = "com.brave.Browser.dev"
