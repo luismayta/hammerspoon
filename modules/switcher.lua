3198f81c9499f042749f3d6d14fbdba3e92e0f8b
@slovacus <slovacus@gmail.com>
2019-12-28 09:36:31 -0500

    (#12) style: format code with editconfig and prettier

--- modules/switcher.lua
+++ modules/switcher.lua
@@ -2,8 +2,8 @@ local hotkey = require('core.hotkey')
 local switcher = hs.window.switcher.new()
 
 hotkey.bindWithAlt(
-   'tab', 'Loading switcher',
-   function()
+  'tab', 'Loading switcher',
+  function()
       switcher:next()
-   end
+  end
 )
