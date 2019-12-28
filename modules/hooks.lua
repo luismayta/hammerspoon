e307bccb1e536ce26aad37428981f269554eb085
@slovacus <slovacus@gmail.com>
2019-12-28 08:46:07 -0500

    chore: implement module hooks (#12)

---
+++ modules/hooks.lua
@@ -0,0 +1,29 @@
+require("core.fntools")
+local settings = require('core.config')
+hs.loadSpoon("SpoonInstall")
+Install=spoon.SpoonInstall
+
+local hooks = {
+
+}
+
+-- USBDeviceActions, opens/closes apps or runs an arbitrary function when a USB device is connected/disconnected
+-- https://github.com/malob/USBDeviceActions.spoon
+function hooks.notifyUsb(x)
+  hs.notify.new({title=x}):send()
+  hs.notify.new({title='Hammerspoon Reloaded'}):send()
+end
+
+Install:andUse(
+  "USBDeviceActions",
+  {
+    config = {
+      devices = {
+        MrRobot = { fn = hooks.notifyUsb },
+      }
+    },
+    start = true
+  }
+)
+
+return hooks
