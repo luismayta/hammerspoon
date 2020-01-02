cbb167dc5772276741630c1d885826286395ffab
@slovacus <slovacus@gmail.com>
2020-01-01 21:24:06 -0500

    chore: Implement spoon countdown with 5 minutes (#9)

--- src/modules/tools.lua
+++ src/modules/tools.lua
@@ -9,34 +9,19 @@ Install:andUse(
   }
 )
 
--- Install:andUse(
---    "ClipboardTool",
---    {
---       config = { gridGeometries = { { "6x4" } } },
---       -- hotkeys = {show_grid = {hyper, "g"}},
---       start = true
---    }
--- )
+Install:andUse("CountDown")
 
--- -- Register Hammerspoon ClipboardTool
--- hs.loadSpoon("ClipboardTool")
--- hsclip_keys = {"alt", "V"}
--- spoon.ModalMgr.supervisor:bind(hsclip_keys[1], hsclip_keys[2], 'Launch Hammerspoon ClipboardTool', function() spoon.ClipboardTool:toggleClipboard() end)
+--
+-- CountDown
+--
+hs.hotkey.bind(hotkey.cmdHyper, "T", function ()
+  hs.notify.new({title="Hammerspoon", informativeText="Starting Timer!"}):send()
+  spoon.CountDown:startFor(5)
+end)
 
-
--- Install:andUse(
---    "TextClipboardHistory",
---    {
---       disable = true,
---       config = {
---          show_in_menubar = false,
---       },
---       hotkeys = {
---          toggle_clipboard = { { "cmd", "shift" }, "v" } },
---       start = true,
---    }
--- )
-
+--
+-- Caffeine
+--
 Install:andUse(
   "Caffeine", {
       start = true,
@@ -49,7 +34,7 @@ Install:andUse(
 Install:andUse(
   "HeadphoneAutoPause",
   {
-      start = true
+    start = true
   }
 )
 
