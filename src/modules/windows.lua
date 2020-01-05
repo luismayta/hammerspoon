bf0a7265f39fb95d26f9e0252f80b02a9fbb683c
@slovacus <slovacus@gmail.com>
2020-01-04 19:15:38 -0500

    (#21) refactor: implement toogle maximized

--- src/modules/windows.lua
+++ src/modules/windows.lua
@@ -1,42 +1,11 @@
-require("src.core.extensions")
+-- require("src.core.extensions")
 local hotkey = require("src.core.hotkey")
+local window = require("hs.window")
 
-hs.loadSpoon("SpoonInstall")
-Install=spoon.SpoonInstall
-Install:andUse(
-  "ModalMgr",
-  {
-      loglevel = 'debug',
-  }
-)
-
-Install:andUse(
-  "WindowHalfsAndThirds",
-  {
-      config = {
-        use_frame_correctness = true
-      },
-      hotkeys = 'default'
-  }
-)
-
-Install:andUse(
-  "WindowScreenLeftAndRight",
-  {
-      hotkeys = 'default'
-  }
-)
-
-Install:andUse(
-  "WindowGrid",
-  {
-      config = { gridGeometries = { { "6x4" } } },
-      hotkeys = {show_grid = {hotkey.hyper, "g"}},
-      start = true
-  }
-)
 hotkey.bindWithAlt(
-  'm', 'Full Screen', fullScreenCurrent
+  'm', 'Toggle Full Screen', function ()
+    window.focusedWindow():toggleFullScreen()
+  end
 )
 -- hs.hotkey.bind(hyper, "D", screenToRight)
 -- hs.hotkey.bind(hyper, "A", screenToLeft)
