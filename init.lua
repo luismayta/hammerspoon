aa3bfeb4645b12b293228d9eb574ee000a5ea3ad
@slovacus <slovacus@gmail.com>
2020-01-04 19:13:16 -0500

    feat: implement speak for i am online (#21)

--- init.lua
+++ init.lua
@@ -3,11 +3,13 @@ spoon.SpoonInstall.repos.doiken = {
   url = "https://github.com/doiken/Spoons",
   desc = "doiken's spoon repository",
 }
+local speech = require 'hs.speech'
+-- Init speaker.
+speaker = speech.new()
 require('src.modules.default')
 require('src.modules.memory')
 require('src.modules.speed')
 require('src.modules.widget')
-require('src.modules.tools')
 require('src.modules.windows')
 require('src.modules.switcher')
 require('src.modules.notification_center')
@@ -15,3 +17,7 @@ require('src.modules.wifi')
 require('src.modules.hooks')
 require('src.modules.browser')
 require('src.modules.apps')
+require('src.modules.tools')
+
+-- Speak something after configuration success.
+speaker:speak(os.getenv("USER") .. ", I am online!")
