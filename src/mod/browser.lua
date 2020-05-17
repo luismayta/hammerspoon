6c055fd647f64ab4c871ba587b5893fa85622ed0
@slovacus <slovacus@gmail.com>
2020-05-17 01:55:37 -0500

    refactor: path modules to mod (#28)

--- src/mod/browser.lua
+++ src/mod/browser.lua
@@ -4,16 +4,16 @@ local settings = require("src.core.config")
 hs.loadSpoon("SpoonInstall")
 Install=spoon.SpoonInstall
 
-Install:andUse("URLDispatcher",
-    {
-        config = {
-            url_patterns = {
-                { "https?://endustria.atlassian.net/", settings.DefaultBrowser },
-                { "https?://bitbucket.org/dashboard/overview",settings.DefaultBrowser },
-                { "https?://github.com", settings.DefaultBrowser },
-            },
-            default_handler = settings.DefaultBrowser
-        },
-        start = true
-    }
-)
+-- Install:andUse("URLDispatcher",
+--     {
+--         config = {
+--             url_patterns = {
+--                 { "https?://endustria.atlassian.net/", settings.DefaultBrowser },
+--                 { "https?://bitbucket.org/dashboard/overview",settings.DefaultBrowser },
+--                 { "https?://github.com", settings.DefaultBrowser },
+--             },
+--             default_handler = settings.DefaultBrowser
+--         },
+--         start = true
+--     }
+-- )
