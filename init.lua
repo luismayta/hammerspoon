cc0caec935da3d57d63984ca1bec582b9f8bd251
@slovacus <slovacus@gmail.com>
2020-09-16 00:33:23 -0500

    chore: implement module vim (#14)

--- init.lua
+++ init.lua
@@ -7,17 +7,18 @@ local speech = require 'hs.speech'
 -- Init speaker.
 speaker = speech.new()
 require('mod.default')
-require('mod.memory')
-require('mod.speed')
+-- require('mod.Memory')
+-- require('mod.speed')
 require('mod.widget')
 require('mod.windows')
 require('mod.switcher')
-require('mod.notification_center')
+-- require('mod.notification_center')
 require('mod.wifi')
 require('mod.hooks')
 require('mod.browser')
 require('mod.apps')
 require('mod.tools')
+require('mod.vim')
 
 -- Speak something after configuration success.
 speaker:speak(os.getenv("USER") .. ", I am online!")
