d087cbd322dbe52ba01a5f68597eea203ebf1c55
Luis Mayta <luis@hadenlabs.com>
2024-02-18 04:11:50 -0300

    fix: implement configuration to modules (#236)

--- mod/wifi.lua
+++ mod/wifi.lua
@@ -1,77 +1,94 @@
 -- luacheck: globals hs spoon
-local fntools = require("core.functions")
-local settings = require("core.config")
 
--- hs.loadSpoon("SpoonInstall")
-local spoon_install = spoon.SpoonInstall
+local logger = require("hs.logger")
 
-spoon_install:andUse("WiFiTransitions", {
-  config = {
-    actions = {
-      -- { -- Test action just to see the SSID transitions
-      --    fn = function(_, _, prev_ssid, new_ssid)
-      --       hs.notify.show("SSID change", string.format("From '%s' to '%s'", prev_ssid, new_ssid), "")
-      --    end
-      -- },
-      {
-        to = "Evilcorp",
-        fn = function(_, _, _, _)
-          hs.fnutils.partial(fntools.reconfigVolume, 80)
-          hs.execute(settings.DnsEmpty)
-          hs.execute(settings.DnsCloudflare)
-        end,
-      },
-      {
-        from = "ulwifiT",
-        fn = function(_, _, _, _)
-          hs.fnutils.partial(fntools.reconfigVolume, 25)
-          hs.execute(settings.DnsEmpty)
-          hs.execute(settings.DnsCloudflare)
-        end,
-      },
-      {
-        from = "ulwifi",
-        fn = function(_, _, _, _)
-          hs.fnutils.partial(fntools.reconfigVolume, 25)
-          hs.execute(settings.DnsEmpty)
-          hs.execute(settings.DnsCloudflare)
-        end,
-      },
-      {
-        to = "ulwifiT",
-        fn = function(_, _, _, _)
-          hs.execute(settings.DnsEmpty)
-        end,
-      },
-      {
-        to = "ulwifi",
-        fn = function(_, _, _, _)
-          hs.execute(settings.DnsEmpty)
-        end,
-      },
-      {
-        to = "Wayra 5G",
-        fn = function(_, _, _, _)
-          hs.fnutils.partial(fntools.reconfigVolume, 50)
-          hs.execute(settings.DnsEmpty)
-        end,
-      },
-      {
-        to = "VIPAC-INVITADOS",
-        fn = function(_, _, _, _)
-          hs.fnutils.partial(fntools.reconfigVolume, 50)
-          hs.execute(settings.DnsEmpty)
-        end,
-      },
-      {
-        from = "VIPAC-INVITADOS",
-        fn = function(_, _, _, _)
-          hs.fnutils.partial(fntools.reconfigVolume, 25)
-          hs.execute(settings.DnsEmpty)
-          hs.execute(settings.DnsCloudflare)
-        end,
+local function init(config)
+  local fntools = require("core.functions")
+  local settings = config
+  local log = logger.new("Apps")
+  -- Load SpoonInstall
+  log.df("SpoonInstall Starting")
+  local success, spoonInstall = pcall(require, "SpoonInstall")
+  if not success then
+      log.ef("Error: in load SpoonInstall")
+      return
+  end
+
+  local Install = spoonInstall
+
+  Install:andUse("WiFiTransitions", {
+    config = {
+      actions = {
+        -- { -- Test action just to see the SSID transitions
+        --    fn = function(_, _, prev_ssid, new_ssid)
+        --       hs.notify.show("SSID change", string.format("From '%s' to '%s'", prev_ssid, new_ssid), "")
+        --    end
+        -- },
+        {
+          to = "Evilcorp",
+          fn = function(_, _, _, _)
+            hs.fnutils.partial(fntools.reconfigVolume, 80)
+            hs.execute(settings.DnsEmpty)
+            hs.execute(settings.DnsCloudflare)
+          end,
+        },
+        {
+          from = "ulwifiT",
+          fn = function(_, _, _, _)
+            hs.fnutils.partial(fntools.reconfigVolume, 25)
+            hs.execute(settings.DnsEmpty)
+            hs.execute(settings.DnsCloudflare)
+          end,
+        },
+        {
+          from = "ulwifi",
+          fn = function(_, _, _, _)
+            hs.fnutils.partial(fntools.reconfigVolume, 25)
+            hs.execute(settings.DnsEmpty)
+            hs.execute(settings.DnsCloudflare)
+          end,
+        },
+        {
+          to = "ulwifiT",
+          fn = function(_, _, _, _)
+            hs.execute(settings.DnsEmpty)
+          end,
+        },
+        {
+          to = "ulwifi",
+          fn = function(_, _, _, _)
+            hs.execute(settings.DnsEmpty)
+          end,
+        },
+        {
+          to = "Wayra 5G",
+          fn = function(_, _, _, _)
+            hs.fnutils.partial(fntools.reconfigVolume, 50)
+            hs.execute(settings.DnsEmpty)
+          end,
+        },
+        {
+          to = "VIPAC-INVITADOS",
+          fn = function(_, _, _, _)
+            hs.fnutils.partial(fntools.reconfigVolume, 50)
+            hs.execute(settings.DnsEmpty)
+          end,
+        },
+        {
+          from = "VIPAC-INVITADOS",
+          fn = function(_, _, _, _)
+            hs.fnutils.partial(fntools.reconfigVolume, 25)
+            hs.execute(settings.DnsEmpty)
+            hs.execute(settings.DnsCloudflare)
+          end,
+        },
       },
     },
-  },
-  start = true,
-})
+    start = true,
+  })
+
+  log.df("SpoonInstall Load Success")
+
+end
+
+return init
