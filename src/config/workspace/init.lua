6571cf729b77d957235970f2b79ed8ad5fdf6e66
Luis Mayta <luis@hadenlabs.com>
2026-02-15 11:00:15 -0500

    refactor 🎨 (core): change apps for workspace (#452)
    Signed-off-by: Luis Mayta <luis@hadenlabs.com>

--- src/config/workspace/init.lua
+++ src/config/workspace/init.lua
@@ -24,47 +24,38 @@ config.hotkeys = {
 }
 
 config.apps = {
+  { mods = { "ctrl", "alt", "cmd" }, key = "[", app = "Android Studio" },
+  { mods = { "ctrl", "alt", "cmd" }, key = "]", app = "IntelliJ IDEA CE" },
+  { mods = { "ctrl", "alt", "cmd" }, key = ";", app = "Zed" },
   { mods = { "ctrl", "alt", "cmd" }, key = "b", app = "Bitwarden" },
-  { mods = { "ctrl", "alt", "cmd" }, key = "e", app = "Vscodium" },
+  { mods = { "ctrl", "alt", "cmd" }, key = "d", app = "draw.io" },
   { mods = { "ctrl", "alt", "cmd" }, key = "t", app = "Ghostty" },
-  { mods = { "ctrl", "alt", "cmd" }, key = "n", app = "Notion" },
   { mods = { "ctrl", "alt", "cmd" }, key = "x", app = "XCode" },
   { mods = { "ctrl", "alt", "cmd" }, key = "i", app = "Insomnia" },
+  { mods = { "ctrl", "alt", "cmd" }, key = "q", app = "DBeaver" },
   { mods = { "ctrl", "alt", "cmd" }, key = "k", app = "Keybase" },
-  { mods = { "ctrl", "alt", "cmd" }, key = "s", app = "Slack" },
   { mods = { "ctrl", "alt", "cmd" }, key = "j", app = "Jira" },
-  { mods = { "ctrl", "alt", "cmd" }, key = "v", app = "Miro" },
-  { mods = { "ctrl", "alt", "cmd" }, key = "z", app = "Zoom.us" },
+  { mods = { "ctrl", "alt", "cmd" }, key = "m", app = "Miro" },
   { mods = { "ctrl", "alt", "cmd" }, key = "o", app = "Obsidian" },
+  { mods = { "ctrl", "alt", "cmd" }, key = "z", app = "Zoom.us" },
 
   { mods = { "ctrl", "alt" }, key = ",", app = "System Preferences" },
   { mods = { "ctrl", "alt" }, key = "3", app = "Launchpad" },
   { mods = { "ctrl", "alt" }, key = "d", app = "Discord" },
   { mods = { "ctrl", "alt" }, key = "f", app = "Figma" },
   { mods = { "ctrl", "alt" }, key = "e", app = "Spark" },
-  { mods = { "ctrl", "alt" }, key = "a", app = "Airtable" },
+  { mods = { "ctrl", "alt" }, key = "a", app = "Arc" },
   { mods = { "ctrl", "alt" }, key = "b", app = "Brave Browser" },
-  { mods = { "ctrl", "alt" }, key = "c", app = "GoogleCalendar" },
+  { mods = { "ctrl", "alt" }, key = "c", app = "Calendar" },
   { mods = { "ctrl", "alt" }, key = "h", app = "WhatsApp" },
   { mods = { "ctrl", "alt" }, key = "t", app = "Telegram" },
-  { mods = { "ctrl", "alt" }, key = "m", app = "Spotify" },
+  { mods = { "ctrl", "alt" }, key = "m", app = "Music" },
   { mods = { "ctrl", "alt" }, key = "r", app = "Reminders" },
   { mods = { "ctrl", "alt" }, key = "o", app = "Finder" },
-}
+  { mods = { "ctrl", "alt" }, key = "s", app = "Stocks" },
 
--- config.apps = {
---   devs = {
---     { key = "t", name = "Ghostty" },
---     { key = "e", name = "Vscodium" },
---   },
---   apps = {
---     { key = "d", name = "Discord" },
---     { key = "b", name = "Brave Browser" },
---   },
---   misc = {
---     { key = "o", name = "Obsidian" },
---   },
--- }
+  { mods = { "shift", "cmd" }, key = "b", app = "Binance" },
+}
 
 config.spoons = {
   {
