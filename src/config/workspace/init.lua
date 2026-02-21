784ce705a93f7c4d25ac54f08bb21c9f3a51f398
Luis Mayta <luis@hadenlabs.com>
2026-02-21 05:58:51 -0500

    feat ✨ (ai): implement tool for opencode (#457)
    Signed-off-by: Luis Mayta <luis@hadenlabs.com>

--- src/config/workspace/init.lua
+++ src/config/workspace/init.lua
@@ -39,22 +39,28 @@ config.apps = {
   { mods = { "ctrl", "alt", "cmd" }, key = "o", app = "Obsidian" },
   { mods = { "ctrl", "alt", "cmd" }, key = "z", app = "Zoom.us" },
 
-  { mods = { "ctrl", "alt" }, key = ",", app = "System Preferences" },
-  { mods = { "ctrl", "alt" }, key = "3", app = "Launchpad" },
-  { mods = { "ctrl", "alt" }, key = "d", app = "Discord" },
-  { mods = { "ctrl", "alt" }, key = "f", app = "Figma" },
-  { mods = { "ctrl", "alt" }, key = "e", app = "Spark" },
-  { mods = { "ctrl", "alt" }, key = "a", app = "Arc" },
-  { mods = { "ctrl", "alt" }, key = "b", app = "Brave Browser" },
-  { mods = { "ctrl", "alt" }, key = "c", app = "Calendar" },
-  { mods = { "ctrl", "alt" }, key = "h", app = "WhatsApp" },
-  { mods = { "ctrl", "alt" }, key = "t", app = "Telegram" },
-  { mods = { "ctrl", "alt" }, key = "m", app = "Music" },
-  { mods = { "ctrl", "alt" }, key = "r", app = "Reminders" },
-  { mods = { "ctrl", "alt" }, key = "o", app = "Finder" },
-  { mods = { "ctrl", "alt" }, key = "s", app = "Stocks" },
+  -- Ai Tools
 
-  { mods = { "shift", "cmd" }, key = "b", app = "Binance" },
+  { mods = { "ctrl", "alt", "cmd" }, key = "p", app = "OpenCode" },
+
+  -- Others Tools
+
+  { mods = { "ctrl", "alt" },        key = ",", app = "System Preferences" },
+  { mods = { "ctrl", "alt" },        key = "3", app = "Launchpad" },
+  { mods = { "ctrl", "alt" },        key = "d", app = "Discord" },
+  { mods = { "ctrl", "alt" },        key = "f", app = "Figma" },
+  { mods = { "ctrl", "alt" },        key = "e", app = "Spark" },
+  { mods = { "ctrl", "alt" },        key = "a", app = "Arc" },
+  { mods = { "ctrl", "alt" },        key = "b", app = "Brave Browser" },
+  { mods = { "ctrl", "alt" },        key = "c", app = "Calendar" },
+  { mods = { "ctrl", "alt" },        key = "h", app = "WhatsApp" },
+  { mods = { "ctrl", "alt" },        key = "t", app = "Telegram" },
+  { mods = { "ctrl", "alt" },        key = "m", app = "Music" },
+  { mods = { "ctrl", "alt" },        key = "r", app = "Reminders" },
+  { mods = { "ctrl", "alt" },        key = "o", app = "Finder" },
+  { mods = { "ctrl", "alt" },        key = "s", app = "Stocks" },
+
+  { mods = { "shift", "cmd" },       key = "b", app = "Binance" },
 }
 
 config.spoons = {
